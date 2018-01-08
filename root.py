aab523e716f75c4d008c413ac1ddcf8c0be0c559
Chris <cms@dreaming.org>
2018-01-08 13:10:42 -0500

    Complete recursive population
    Trunk to nodes to services to resources.

--- root.py
+++ root.py
@@ -8,30 +8,37 @@ class Root():
 		self.name = name
 		self.table = table
 		self.branch_table = branch_table
-		self.branches = list()
+		self.branches = {}
 
-	def getBranches(table, xref_id=1, xref_table='active'):
-		sql = '''SELECT * FROM ''' + table + ''' WHERE ''' + xref_table + '''=:xref_id;'''
+	def getBranches(table, xref_id=1, xref_col='active'):
+		sql = '''SELECT * FROM ''' + table + ''' WHERE ''' + xref_col + '''=:xref_id;'''
 		args = {"xref_id": xref_id}
-		print (sql, args)
+		#print (sql, args)
 		resources = db.query(sql, args)
 		return resources
 
+	def addBranches(self, branches):
+		for branch in branches:
+			self.branches.update({branch[0]: getattr(sys.modules[__name__], self.branch_table.capitalize())(branch[0], branch[1])})
+
 	def __str__(self):
-		return "%s id: %i name: %s branch_table: %s " % (self.table, self.id, self.name, self.branch_table)
+		return "\t\t%s id: %i name: %s branch_table: %s " % (self.table, self.id, self.name, self.branch_table)
 
 	def listBranches(self):
-		for branch in self.branches:
+		print (self.name + " \\")
+		for key, branch in self.branches.items():
 			print (branch)
 
+	def iterBranches(self, branch_id, branch_table):
+		print("not yet")
 
-class Viewer(Root):
+
+class Trunk(Root):
 
 	def __init__(self):
-		Root.__init__(self, 0, 'Viewer', 'node')
+		Root.__init__(self, 0, 'Trunk', 'node')
 		branches = Root.getBranches(self.branch_table)
-		for branch in branches:
-			self.branches.append(getattr(sys.modules[__name__], self.branch_table.capitalize())(branch[0], branch[1]))
+		Root.addBranches(self, branches)
 
 
 class Node(Root):
@@ -39,21 +46,23 @@ class Node(Root):
 	def __init__(self, id, name):
 		Root.__init__(self, id, name, 'service', 'node')
 		branches = Root.getBranches(self.branch_table, self.id, self.table + "_id")
-		for branch in branches:
-			self.branches.append(getattr(sys.modules[__name__], self.branch_table.capitalize())(branch[0], branch[1]))
+		Root.addBranches(self, branches)
+		self.listBranches()
 
 
 class Service(Root):
 
 	def __init__(self, id, name):
 		Root.__init__(self, id, name, 'resource', 'service')
-
+		branches = Root.getBranches(self.branch_table, self.id, self.table + "_id")
+		Root.addBranches(self, branches)
+		self.listBranches()
 
 class Resource(Root):
 
-	def nothing(self):
-		print("nothing")
+	def __init__(self, id, name):
+		Root.__init__(self, id, name, None, 'resource')
 
 
-view = Viewer()
-view.listBranches()
+trunk = Trunk()
+trunk.listBranches()
